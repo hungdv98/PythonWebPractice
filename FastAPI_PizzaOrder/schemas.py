6201821e825e1dc422a7b76e38b782f124c990d7
Đặng Việt Hưng <packetloss0709@gmail.com>
2022-03-19 17:42:56 +0700

    [API] crud orders

--- FastAPI_PizzaOrder/schemas.py
+++ FastAPI_PizzaOrder/schemas.py
@@ -1,3 +1,4 @@
+from optparse import Option
 from pydantic import BaseModel
 from typing import Optional
 
@@ -27,3 +28,30 @@ class Settings(BaseModel):
 class LoginModel(BaseModel):
     username:str
     password:str
+
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str] = "PENDING"
+    pizza_size:Optional[str] = "SMALL"
+    user_id:Optional[int]
+
+    class Config:
+        orm_mode = True 
+        schema_extra = {
+            "example": {
+                "quantity": 2,
+                "pizza_size": "LARGE"
+            }
+        }
+
+class OrderStatusModel(BaseModel):
+    order_status:Optional[str] = "PENDING"
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "order_status": "PENDING"
+            }
+        }
