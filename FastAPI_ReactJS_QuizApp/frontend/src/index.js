fe6f069e938ddeb69fb0cd980b44ee6f318e5b84
hungdv98 <packetloss0709@gmail.com>
2022-04-13 01:35:11 +0700

    [FE] init FE

--- FastAPI_ReactJS_QuizApp/frontend/src/index.js
+++ FastAPI_ReactJS_QuizApp/frontend/src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode, useState, useEffect } from 'react'
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    useEffect(
+        () => {
+            fetch("/auth/noauthen")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setMessage(data.message)
+            })
+            .then(err => console.log(err))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        },[]
+    )
+    const [message, setMessage] = useState("");
+    return(
+        <div className="container">
+            <h1>{message}</h1>
+        </div>
+    )
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
