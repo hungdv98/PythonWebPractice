d8e187b342223f06426d4e8ad10e4e5356afe1df
hungdv98 <packetloss0709@gmail.com>
2022-04-17 23:42:44 +0700

    [FE] download qrcode

--- FastAPI_ReactJS_QuizApp/ruler/src/components/CreateQR.js
+++ FastAPI_ReactJS_QuizApp/ruler/src/components/CreateQR.js
@@ -2,10 +2,24 @@
  //http://localhost:3000/?uid=17&eid=1
 
 import QRCode from 'qrcode.react';
+import { useState } from 'react';
+
+const downloadQR = () => {
+    const canvas = document.getElementById('qrcode');
+    const pngUrl = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+    console.log('pngUrl', pngUrl);
+    let downloadLink = document.createElement('a');
+    downloadLink.href = pngUrl;
+    downloadLink.download = 'vsec_exam.png';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+};
 
 const CreateQRPage = () => {
 
-
+    const [uid, setUid] = useState(17);
+    const [eid, setEid] = useState(1);
 
     return(
         <div>
@@ -16,6 +30,8 @@ const CreateQRPage = () => {
                 level={'H'}
                 includeMargin={true}
             />
+            <br />
+            <a onClick={downloadQR}> Download QR </a>
         </div>
     )
 }
